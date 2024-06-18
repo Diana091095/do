5079196a903a7f477eb2e2f1ecf3f2fc39801c54
Diana091095 <diana091095@mail.ru>
2024-06-18 22:14:46 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+print('hello Doctor')
+print(len('hello Doctor')) # Len-считает длину строки
+
+print('5','6')
+print(5+6, 5-6)
+print('summa=11','diff=-1')
+print(type('summa 11'), type('diff -1'))
+
+print('4', '6', '8')
+print((4+6+8)//3)
+print('mean=', (4+6+8)//3)
+
+print('Понедельник,',' Вторник')
+
+print('a=1', 'b=3', 'c=5', 'f=x')
+print('x=(a*b)+(a*c)', 'x=', '(1*3)+(1*5)', 'x=', (1*3)+(1*5))
+print((8**3)/2)
